7d14f19bbe3fdc25f772d46b5c6cb863c4227e3b
Oscar Gonzalez <ogonzal87@gmail.com>
2015-02-02 23:14:24 -0500

    tested static page about

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,4 +9,13 @@ describe 'static pages' do
     it { should have_title('Manly Drinks') }
     it { should have_selector('h1', text: 'Manly!') }
   end
+
+  describe 'about page' do
+  	before { visit root_path } # i am not sure why this this failing... it tells me that 'visit' is an undefined method when i run rpec..
+
+  	it { should have_title('About') }
+  	it { should have selector('p', text: 'Food') }
+  end
+
 end
+
